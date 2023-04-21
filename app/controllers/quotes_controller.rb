436e5b886559b6514d52aa95ae264c8b0baa1fbd
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-04-21 05:54:22 +0100

    feat(Quotes): define `new` and `create` actions

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,4 +5,18 @@ class QuotesController < ApplicationController
 
   def show
   end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to quotes_path, notice: "Quote was successfully created."
+    else
+      render :new
+    end
+  end
 end
