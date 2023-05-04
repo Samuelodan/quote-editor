8428c106479aa2c0a0fcc7bdd8b121c7b36c9315
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-05-05 00:23:00 +0100

    test(quotes_test): modify "update quote" system test
    the app will no longer go to an "update" page, but will instead bring the content of that page to the index page where the "edit" button is.

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -31,9 +31,9 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
